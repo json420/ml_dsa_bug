e847955d561cbf8d3ed3df2abcd7cd5c2d00f48a
Jason Gerard DeRose <jasonderose@gmail.com>
2025-02-11 11:23:56 -0700

    Ok, failure is on round tripping the signature

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use blake3::{hash, Hash};
-use ml_dsa::{KeyGen, MlDsa65, B32};
+use ml_dsa::{
+    EncodedSignature, EncodedVerifyingKey, KeyGen, MlDsa65, Signature, VerifyingKey, B32,
+};
 use signature::{Signer, Verifier};
 
 #[cfg(test)]
@@ -19,6 +21,13 @@ mod tests {
             assert!(kp.verifying_key.verify(msg, &sig).is_ok());
             if i == 41 {
                 assert_eq!(
+                    hash(kp.signing_key.encode().as_slice()),
+                    Hash::from_hex(
+                        "1d2cd0942ff8ff41356b0951f1dc18fe76d2c7b1bd8ccfdc3bf7bdaa456a590b"
+                    )
+                    .unwrap()
+                );
+                assert_eq!(
                     hash(kp.verifying_key.encode().as_slice()),
                     Hash::from_hex(
                         "f006583eeaa8820eb9184a72324ca5259251ae915c5655cda5afb9dcd9982dc0"
@@ -33,6 +42,16 @@ mod tests {
                     .unwrap()
                 );
             }
+
+            let mut pub_buf = [0; 1952];
+            pub_buf.copy_from_slice(kp.verifying_key.encode().as_slice());
+            let mut sig_buf = [0; 3309];
+            sig_buf.copy_from_slice(sig.encode().as_slice());
+            println!("{}", i);
+            let pub_enc = EncodedVerifyingKey::<MlDsa65>::try_from(&pub_buf[..]).unwrap();
+            let pub_key = VerifyingKey::<MlDsa65>::decode(&pub_enc);
+            let sig_enc = EncodedSignature::<MlDsa65>::try_from(&sig_buf[..]).unwrap();
+            let sig = Signature::<MlDsa65>::decode(&sig_enc).unwrap(); // This will fail at i=16
         }
     }
 }
