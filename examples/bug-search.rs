fe37984b4994582ce26567a3c4555188d57449f0
Jason Gerard DeRose <jasonderose@gmail.com>
2025-02-12 09:43:33 -0700

    Few tweaks to bug-search example

--- examples/bug-search.rs
+++ examples/bug-search.rs
@@ -8,7 +8,7 @@ const COUNT: usize = 10_000_000;
 static MESSAGE: &[u8] = b"There seems to be a round tripping issue somewhere in here";
 
 fn main() {
-    let mut seed = Hash::from_bytes([69; 32]);
+    let mut seed = Hash::from_bytes([77; 32]);
     for i in 0..COUNT {
         seed = hash(seed.as_bytes());
         let mut hack = B32::default();
@@ -16,7 +16,12 @@ fn main() {
         let kp = MlDsa65::key_gen_internal(&hack);
         let sig = kp.signing_key().sign(MESSAGE);
         let sig_enc = sig.encode();
-        println!("{} {}", seed, i);
-        let _sig = Signature::<MlDsa65>::decode(&sig_enc).unwrap();
+        if Signature::<MlDsa65>::decode(&sig_enc).is_none() {
+            println!("FAIL! {} {}", seed, i);
+            break;
+        }
+        if i % 10_000 == 0 {
+            println!("{} {}", seed, i);
+        }
     }
 }
